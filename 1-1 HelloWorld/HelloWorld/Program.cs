6a5956395ed2cbf16a8558d40cdc4019eaf078ef
Soraneera <85293087+Soraneera@users.noreply.github.com>
2021-06-07 18:55:38 +0200

    Add files via upload

---
+++ 1-1 HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Keyword "Console": een console aanspreken
+            // Writeline: schrijft tekst naar scherm + begint een nieuwe regel
+            // ReadLine: cursor blijft op scherm
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine("Druk op enter om het console af te sluiten");
+        }
+    }
+}
